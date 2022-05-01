53f8639967d25be6582c681538174b7e7d697f14
JinChaojjj <3125545791@qq.com>
2022-05-01 23:56:37 +0800

    twice

---
+++ src/section1/Test1.java
@@ -0,0 +1,12 @@
+package section1;
+
+/**
+ * @author 金超
+ * @description: TODO
+ * @date 2022-05-01
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println(11);
+    }
+}
