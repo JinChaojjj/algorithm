d8c46c0e9fe9104c3beb8a42806d9a64f62a5074
JinChaojjj <3125545791@qq.com>
2022-05-01 23:51:18 +0800

    first

---
+++ src/section1/unit1_1.java
@@ -0,0 +1,24 @@
+package section1;
+
+/**
+ * @author 金超
+ * @description: TODO
+ * @date 2022-03-12
+ */
+public class unit1_1 {
+    public static void main(String[] args) {
+        // 1.1.1
+        System.out.println((0+15)/2);
+        System.out.println(2.0e-6*100000000.1);
+        System.out.println(true && false || true && false);
+
+        // 1.1.2
+        System.out.println((1+2.236)/2);
+        System.out.println(1+2+3+4.0);
+        System.out.println(4.1 >= 4);
+        System.out.println(1+2+"3");
+
+        //1.1.8
+        System.out.println('a'+'b');
+    }
+}
